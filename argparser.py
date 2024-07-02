3b40b31e77efa3a3351c594612bf4551190d551b
peppermin-t <chenyinjia2000@gmail.com>
2024-07-02 15:54:24 +0100

    add to device

--- argparser.py
+++ argparser.py
@@ -4,7 +4,7 @@ def parsearg():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dsclass', default='easy', type=str, help='Benchmark class')
-    parser.add_argument('--filename', default='pseudoweighted_emptyroom_d4_g2_p_t2.cnf', type=str, help='Name of the file')
+    parser.add_argument('--filename', default='bayes_4step.cnf', type=str, help='Name of the file')
     parser.add_argument('--modelpth', default='models/easy', type=str, help='Path of models')
 
     parser.add_argument('--format', type=str, choices=['CAC', 'MIN', 'UNW', 'TRA'], default='MIN', help='CNF file format')
